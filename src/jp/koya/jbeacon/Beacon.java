406a6c7bab2b2acb527ba618a9027abf2f25d3bb
Koya <koya0306@outlook.jp>
2018-08-18 10:13:44 +0900

    リファクター

--- src/jp/koya/jbeacon/Beacon.java
+++ src/jp/koya/jbeacon/Beacon.java
@@ -10,17 +10,47 @@ public class Beacon {
     static {
 	Platform.loadLibrary(System.getProperty("user.dir") + "/beacon");
     }
+    /**
+     * 現在の jbeacon の状態.
+     */
+    public enum State {
+	None,
+	Initialized,
+	Destroyed
+    }
+    private static State state = State.None;
     
+    /**
+     * jbeacon を初期化します.
+     */
     public static void init() {
+	if(state != State.None) {
+	    throw new IllegalStateException();
+	}
 	nativeInit();
+	state = State.Initialized;
     }
     
+    /**
+     * 指定のファイルを実行します.
+     * @param filename 
+     */
     public static void execute(String filename) {
+	if(state != State.Initialized) {
+	    throw new IllegalStateException();
+	}
 	nativeExecute(filename);
     }
     
+    /**
+     * jbeacon を終了します.
+     */
     public static void destroy() {
+	if(state != State.Initialized) {
+	    throw new IllegalStateException();
+	}
 	nativeDestroy();
+	state = State.Destroyed;
     }
     
     private native static void nativeInit();
